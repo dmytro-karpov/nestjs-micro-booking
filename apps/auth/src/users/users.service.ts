b6c389e0ae4f02081ab63becae911b182372784d
Dmytro Karpov <dmy.karpov@gmail.com>
2024-06-25 17:24:48 +0200

    Adds local auth strategy

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
+
   async create(createUserDto: CreateUserDto) {
-    return this.usersRepository.create(createUserDto);
+    return this.usersRepository.create({
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    });
+  }
+
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const passworIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passworIsValid) {
+      throw new Error('Credentials are not valid.');
+    }
+
+    return user;
+  }
+
+  async getUser(getUserDto: GetUserDto) {
+    return this.usersRepository.findOne(getUserDto);
   }
 }
