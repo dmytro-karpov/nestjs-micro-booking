8e3f87f85012112285ae4c62a21167eda3b7f0cf
Dmytro Karpov <dmy.karpov@gmail.com>
2024-06-25 09:05:47 +0200

    Adds Docker

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -10,7 +10,6 @@ import * as Joi from 'joi';
     NestConfigModule.forRoot({
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
-        MONGODB_DB: Joi.string().required(),
       }),
     }),
   ],
